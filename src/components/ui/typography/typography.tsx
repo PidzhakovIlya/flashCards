680e11f9816063a3407e8109da7be68f3072ab1a
PidzhakovIlya <pidzhakovilja@gmail.com>
2024-05-26 20:28:34 +0300

    add typogaphy polymorf component and add style scss typogaphhy

---
+++ src/components/ui/typography/typography.tsx
@@ -0,0 +1,31 @@
+import { ComponentPropsWithoutRef, ElementType } from 'react'
+
+import s from './typography.module.scss'
+
+export type TypographyProps<T extends ElementType> = {
+  as: T
+  variant:
+    | 'body1'
+    | 'body2'
+    | 'caption'
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'h4'
+    | 'large'
+    | 'link1'
+    | 'link2'
+    | 'overline'
+    | 'subtitle1'
+    | 'subtitle2'
+} & ComponentPropsWithoutRef<T>
+
+export function Typography<T extends ElementType>({
+  as,
+  className,
+  ...restProps
+}: TypographyProps<T>) {
+  const Component = as ?? 'p'
+
+  return <Component className={`${s[variant]} ${className}`} {...restProps} />
+}
